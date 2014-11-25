8cff2218c5084ed83578053ef647a44a56787ac3
Jakub Liput <jakub.liput@gmail.com>
2014-11-25 17:56:32 +0100

    SCAL-450: build refactor

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 export GOPATH=`pwd`
+PACKAGES_DIR=packages/
 
-rm -rf pkg/*
+rm -rf $PACKAGES_DIR/*
 
 for OS in linux; do
     for ARCH in amd64 386; do
-        BIN_PATH="pkg/${OS}_${ARCH}/scalarm_simulation_manager"
+        BIN_PATH="$PACKAGES_DIR/${OS}_${ARCH}/scalarm_simulation_manager"
         echo "Building: $OS $ARCH in ${BIN_PATH}..."
-        GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 go build -o packages/${OS}_${ARCH}/scalarm_simulation_manager scalarm_simulation_manager
+        GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 go build -o $PACKAGES_DIR/${OS}_${ARCH}/scalarm_simulation_manager scalarm_simulation_manager
         strip $BIN_PATH
         xz $BIN_PATH
     done
