5df607c298e1612ac22677de7483b6936be6f597
Jakub Liput <jakub.liput@gmail.com>
2014-11-25 17:29:10 +0100

    SCAL-450: changing build output dir from pkg to packages to avoid names conflict

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ for OS in linux; do
     for ARCH in amd64 386; do
         BIN_PATH="pkg/${OS}_${ARCH}/scalarm_simulation_manager"
         echo "Building: $OS $ARCH in ${BIN_PATH}..."
-        GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 go build -o pkg/${OS}_${ARCH}/scalarm_simulation_manager scalarm_simulation_manager
+        GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 go build -o packages/${OS}_${ARCH}/scalarm_simulation_manager scalarm_simulation_manager
         strip $BIN_PATH
         xz $BIN_PATH
     done
